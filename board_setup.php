0015753f8c902dfd6575b0a5e709a4a7c50d73b3
zelock <118821494+zelock@users.noreply.github.com>
2023-01-25 15:59:08 +0900

    Add files via upload

---
+++ board_setup.php
@@ -0,0 +1,12 @@
+<?php
+    $table = $_GET["table"];
+    $scale = 4;	// 글 목록보기에서 한 화면에 표시되는 글 수
+
+    switch($table) {
+        case "_notice" : $board_title = "공지 게시판";
+                break;
+       
+        case "_qna": $board_title = "글쓰기";
+                break;                      
+    }
+?>
---
+++ db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+    $con = mysqli_connect("localhost", "zelock", "lmw4592!", "love");
+?>
